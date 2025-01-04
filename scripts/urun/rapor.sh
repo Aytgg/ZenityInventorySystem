aaad237f9f742887d2a70efd3c8a09e05dbbe1e8
Aytuğ DEĞER <aytugdeger.2003@gmail.com>
2025-01-04 19:56:47 +0300

    Toplam ürün değeri rapor seçeneği eklendi.

--- scripts/urun/rapor.sh
+++ scripts/urun/rapor.sh
@@ -2,8 +2,9 @@
 
 RAPOR_TURU=$(zenity --list --title="Rapor Türü Seçin"	\
     --column="ID" --column="Rapor Türü"					\
-    1 "Stoğu Azalan Ürünler"	\
-    2 "En Yüksek Stok Miktarına Sahip Ürünler")
+    1 "Stoğu Azalan Ürünler"					\
+    2 "En Yüksek Stok Miktarına Sahip Ürünler"	\
+	3 "Toplam Ürün Değeri")
 
 case $RAPOR_TURU in
     1)	ESIK=$(zenity --entry --title="Eşik Değeri" --text="Eşik stok miktarını girin:")
@@ -12,6 +13,9 @@ case $RAPOR_TURU in
     2)	ESIK=$(zenity --entry --title="Eşik Değeri" --text="Eşik stok miktarını girin:")
         awk -F"," -v esik=$ESIK '$4 > esik {print $0}' csvFiles/depo.csv | zenity --text-info --title="En Yüksek Stok Miktarına Sahip Ürünler"
         ;;
+	3)	total=$(awk -F"," '{total_value += $4 * $5} END {print total_value}' csvFiles/depo.csv)
+    	zenity --info --title="Toplam Değer" --text="Tüm ürünlerin toplam değeri: $total ₺"
+    	;;
     *)	zenity --error --text="Geçersiz seçim."	
 		;;
 esac
